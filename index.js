e725bb861a947bd80985cfa649ce639c38369bbc
AlexPurhalo <alexpurhalo@gmail.com>
2016-12-17 16:12:34 +0200

    question-id-showing

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const xhr = new XMLHttpRequest();
+
 xhr.open('GET', 'http://jservice.io/api/random', false);
 xhr.send();
 
-console.log(xhr.response);
+
+if (xhr.status != 200) {
+	console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+} else {
+	const res = JSON.parse(xhr.responseText);
+	console.log(res[0]['id']);
+	const question = res[0]['id'];
+
+
+	document.getElementsByClassName("question-id")[0].innerHTML = question;
+}
